92a4c344865133dacdf26fbcf7353ad6d46106ad
akkuratpasse <AndreasM@1x-193-157-207-67.uio.no>
2015-09-11 17:35:09 +0200

    utkast v2 1. prosjekt

--- Prosjekt 1/Project1b/main.cpp
+++ Prosjekt 1/Project1b/main.cpp
@@ -8,7 +8,9 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
+// Numerical Solution
     int n = 10;
+//    int n = 100;
 
     // Step length
     double h = 1.0/ ((double) n+1);
@@ -49,20 +51,45 @@ int main(int argc, char** argv)
     cout << f << endl;
 
 
-    // siden a diagonal na er 0 er det mulig a finne hva siste b er i diagonalen
+    // hva siste b er i diagonalen
     v[n-1] = f[n-1]/b[n-1];
-//    cout << v[n-1] << endl; // Skriver ut siste tall pa diagonalen
-//    v[n-1] = f[n-1]/b[n-1]; // versjon 2
+    //cout << v[n-1] << endl; // Skriver ut siste tall pa vektor
 
     // Backward Substitution
     for(int i = n-1; i > 0; i--){
         v[i-1] = (f[i-1]- c[i-1]*v[i])/b[i-1];
     }
+    cout << "v, Numerical Solution" << endl;
+    cout << v << endl;  // det som skrives ut er internal points
 
-    cout << "v" << endl;
-    cout << v << endl;
-    // det som skrives ut er internal points
     v.save("V.txt", raw_ascii);
 
-    //
+// Analytical Solution
+    // int n = 10;
+    // Step length
+    //  double h = 1.0/ ((double) n+1);
+
+    // Use Armadillo to allocate arrays
+    vec u = zeros<vec>(n);
+
+    // Analytical Solution, u
+    for(int i=0; i < n; i++){
+        u[i] = (1-(1-exp(-10))*((i+1)*h)-exp(-10*(i+1)*h));
+    }
+    cout << "u, Analytical Solution" << endl;
+    cout << u << endl;  // det som skrives ut er internal points
+    u.save("U.txt", raw_ascii);
+
+// Relative error
+    vec e = zeros<vec>(n);
+    for(int i=0; i < n; i++){
+    e[i] = log10(abs(((v[i]-u[i])/u[i]))); // vil vare lik
+    cout << "e, Relative Error" << endl;
+    cout << e << endl;
+    e.save("e.txt");
+    }
+
+    // velger ett punkt for e og lager lokke for forksjellig n
+
+
 }
